5214d60c202492b7c7d8e33a51da44f636d027c0
Ditto <id.salazar@uniandes.edu.co>
2021-05-10 21:18:30 -0500

    full code

---
+++ Assets/test.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+	
+	float num = 0.0f;
+	bool a = true;
+	int phase = 0;
+	float red = 0.0f;
+	float green = 1.0f;
+	float blue = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (num > 1.0f){
+			a = false;
+		}
+		if (num < 0.03f){
+			a = true;
+		}
+		
+		if(a){
+			num = num + 0.007f;
+		}else{
+			num = num - 0.007f;
+		}
+		
+		if(phase == 0){
+			blue = blue - 0.01f;
+			if (blue <= 0.0f){
+				phase = 1;
+			}
+		}else if(phase == 1){
+			red = red + 0.01f;
+			if (red >= 1.0f){
+				phase = 2;
+			}
+		}else if(phase == 2){
+			blue = blue + 0.01f;
+			green = green - 0.01f;
+			if (blue >= 1.0f){
+				phase = 3;
+			}
+		}else if(phase == 3){
+			red = red - 0.01f;
+			green = green + 0.01f;
+			if (green >= 1.0f){
+				phase = 0;
+			}
+		}
+		
+		GetComponent<Renderer>().material.shader = Shader.Find("Custom/NewSurfaceShader");
+		GetComponent<Renderer>().material.SetFloat("_ExtrusionFactor", num);
+		
+		GetComponent<Renderer>().material.SetFloat("_Red", red);
+		GetComponent<Renderer>().material.SetFloat("_Blue", blue);
+		GetComponent<Renderer>().material.SetFloat("_Green", green);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CVIProject4
+Proyecto 4 de Computación Visual Interactiva
+
+
+Iván David Salazar Cárdenas - 201629873
+
+
+![Proj 4](https://i.imgur.com/W0W4mAV.png)
+
+Proyecto de Unity, para observar la escena, hacer click en play.
+
+No se incluye la Carpeta Library :(
